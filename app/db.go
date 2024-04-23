97d536945100dd90699037a578a07b5788c5ffbf
maximediet <maximediet>
2024-04-23 17:38:15 +0200

    create sql creation functions

--- app/db.go
+++ app/db.go
@@ -23,29 +23,198 @@ func InitDB(dsn string) *sql.DB {
 }
 
 func SetupDB(db *sql.DB) {
+	createTableUsers(db)
+	createTableSubject(db)
+	createTableTag(db)
+	createTableQuestion(db)
+	createTableResponse(db)
+	createTableTagged(db)
+	createTablePrecise(db)
+	createTableVote_response(db)
+	createTableVote_question(db)
+
+}
+
+func createTableUsers(db *sql.DB) {
 	// Create a User table
 	tableCreationQuery := `CREATE TABLE IF NOT EXISTS users (
-	id_student SERIAL NOT NULL,
-	lastname VARCHAR(50) NOT NULL,
-	firstname VARCHAR(50) NOT NULL,
-	nickname VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL,
-	avatar VARCHAR(50),
-	birth_date DATE,
-	bio VARCHAR(100),
-	website VARCHAR(50),
-	github VARCHAR(50),
-	xp INT,
-	rang_rank_ INT,
-	school_year DATE,
-	creation_date DATE,
-	update_date DATE,
-	deleting_date DATE,
-	PRIMARY KEY(id_student),
-	UNIQUE(nickname),
-	UNIQUE(email)
-);`
+		id_student SERIAL NOT NULL,
+		lastname VARCHAR(50) NOT NULL,
+		firstname VARCHAR(50) NOT NULL,
+		nickname VARCHAR(50) NOT NULL,
+		email VARCHAR(50) NOT NULL,
+		password VARCHAR(50) NOT NULL,
+		avatar VARCHAR(50),
+		birth_date DATE,
+		bio VARCHAR(100),
+		website VARCHAR(50),
+		github VARCHAR(50),
+		xp INT,
+		rang_rank_ INT,
+		school_year DATE,
+		creation_date DATE,
+		update_date DATE,
+		deleting_date DATE,
+		PRIMARY KEY(id_student),
+		UNIQUE(nickname),
+		UNIQUE(email)
+	);`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableSubject(db *sql.DB) {
+	// Create a Subject table
+	tableCreationQuery := `CREATE TABLE Subject(
+		id_subject SERIAL NOT NULL,
+		title VARCHAR(50) NOT NULL,
+		description VARCHAR(500) NOT NULL,
+		creation_date DATE NOT NULL,
+		update_date DATE NOT NULL,
+		PRIMARY KEY(id_subject),
+		UNIQUE(title)
+	);`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableTag(db *sql.DB) {
+	// Create a Tag table
+	tableCreationQuery := `CREATE TABLE Tag(
+		id_tag SERIAL NOT NULL,
+		title VARCHAR(50) NOT NULL,
+		description VARCHAR(500) NOT NULL,
+		creation_date DATE NOT NULL,
+		update_date DATE NOT NULL,
+		PRIMARY KEY(id_tag),
+		UNIQUE(title)
+	 );`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableQuestion(db *sql.DB) {
+	// Create a Question table
+	tableCreationQuery := `CREATE TABLE Question(
+		id_question SERIAL NOT NULL,
+		title VARCHAR(50) NOT NULL,
+		content VARCHAR(500) NOT NULL,
+		upvotes INT,
+		downvotes INT,
+		creation_date DATE NOT NULL,
+		update_date DATE NOT NULL,
+		id_student INT NOT NULL,
+		id_subject INT NOT NULL,
+		PRIMARY KEY(id_question),
+		UNIQUE(title),
+		FOREIGN KEY(id_student) REFERENCES Student(id_student),
+		FOREIGN KEY(id_subject) REFERENCES Subject(id_subject)
+	 );`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableResponse(db *sql.DB) {
+	// Create a Response table
+	tableCreationQuery := `CREATE TABLE Response(
+		id_response SERIAL NOT FULL,
+		content VARCHAR(500) NOT NULL,
+		upvotes INT,
+		downvotes INT,
+		best_answer LOGICAL NOT NULL,
+		creation_date DATE NOT NULL,
+		update_date DATE NOT NULL,
+		id_question INT NOT NULL,
+		id_student INT NOT NULL,
+		PRIMARY KEY(id_response),
+		FOREIGN KEY(id_question) REFERENCES Question(id_question),
+		FOREIGN KEY(id_student) REFERENCES Student(id_student)
+	 );
+	 `
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableTagged(db *sql.DB) {
+	// Create a Tagged table
+	tableCreationQuery := `CREATE TABLE Tagged(
+		id_question INT,
+		id_tag INT,
+		PRIMARY KEY(id_question, id_tag),
+		FOREIGN KEY(id_question) REFERENCES Question(id_question),
+		FOREIGN KEY(id_tag) REFERENCES Tag(id_tag)
+	 );
+	`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTablePrecise(db *sql.DB) {
+	// Create a Precise table
+	tableCreationQuery := `CREATE TABLE Precise(
+		id_subject INT,
+		id_tag INT,
+		PRIMARY KEY(id_subject, id_tag),
+		FOREIGN KEY(id_subject) REFERENCES Subject(id_subject),
+		FOREIGN KEY(id_tag) REFERENCES Tag(id_tag)
+	 );
+	`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableVote_response(db *sql.DB) {
+	// Create a	Vote_response table
+	tableCreationQuery := `CREATE TABLE Vote_response(
+		id_student INT,
+		id_response INT,
+		upvote_r LOGICAL NOT NULL,
+		downvote_r LOGICAL NOT NULL,
+		PRIMARY KEY(id_student, id_response),
+		FOREIGN KEY(id_student) REFERENCES Student(id_student),
+		FOREIGN KEY(id_response) REFERENCES Response(id_response)
+	 );
+	`
+
+	// Execute the table creation query
+	if _, err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTableVote_question(db *sql.DB) {
+	// Create a	Vote_question table
+	tableCreationQuery := `CREATE TABLE Vote_question(
+		id_student INT,
+		id_question INT,
+		upvote_q LOGICAL NOT NULL,
+		downvote_q LOGICAL NOT NULL,
+		PRIMARY KEY(id_student, id_question),
+		FOREIGN KEY(id_student) REFERENCES Student(id_student),
+		FOREIGN KEY(id_question) REFERENCES Question(id_question)
+	 );
+	`
 
 	// Execute the table creation query
 	if _, err := db.Exec(tableCreationQuery); err != nil {
