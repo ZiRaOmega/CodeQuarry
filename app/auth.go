d828e24e97489cc1c541c353b76f0a2c76456413
lemon_42 <juliendasilva9495@gmail.com>
2024-04-23 08:47:07 +0200

    Add logging information for most potential error

--- app/auth.go
+++ app/auth.go
@@ -32,24 +32,28 @@ func RegisterHandler(db *sql.DB) http.HandlerFunc {
 
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
+			Log(ErrorLevel, "Error hashing password")
 			http.Error(w, "Error hashing password", http.StatusInternalServerError)
 			return
 		}
 
 		stmt, err := db.Prepare("INSERT INTO users(fullname, username, email, password) VALUES(?, ?, ?, ?)")
 		if err != nil {
+			Log(ErrorLevel, "Error preparing the SQL statement")
 			http.Error(w, "Error preparing the SQL statement", http.StatusInternalServerError)
 			return
 		}
 		defer stmt.Close()
 
 		if _, err := stmt.Exec(fullname, username, email, string(hashedPassword)); err != nil {
+			Log(ErrorLevel, "Error inserting the data into the database")
 			http.Error(w, "Error inserting the data into the database", http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"status": "success", "message": "Registration successful"})
+		Log(DebugLevel, "Registration successful: "+username+" at "+r.URL.Path)
 	}
 }
 
@@ -73,10 +77,12 @@ func LoginHandler(db *sql.DB) http.HandlerFunc {
 		err := db.QueryRow("SELECT password FROM users WHERE username = ? OR email = ?", username, username).Scan(&storedPassword)
 		if err != nil {
 			if err == sql.ErrNoRows {
+				Log(ErrorLevel, "No user found with the provided credentials"+username+" at "+r.URL.Path)
 				fmt.Println("No user found with the provided credentials")
 				http.Error(w, "No user found with the provided credentials", http.StatusUnauthorized)
 				return
 			} else {
+				Log(ErrorLevel, "Error fetching user from database"+username+" at "+r.URL.Path)
 				fmt.Println("Error fetching user from database")
 				http.Error(w, "Error fetching user from database", http.StatusInternalServerError)
 				return
@@ -86,6 +92,7 @@ func LoginHandler(db *sql.DB) http.HandlerFunc {
 		// Compare the stored hashed password with the password that was submitted
 		err = bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(password))
 		if err != nil {
+			Log(DebugLevel, "Invalid login credentials"+username+" at "+r.URL.Path)
 			fmt.Println("Invalid login credentials")
 			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
 			return
@@ -94,6 +101,8 @@ func LoginHandler(db *sql.DB) http.HandlerFunc {
 		// Login successful
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"status": "success", "message": "Login successful"})
+		Log(DebugLevel, "Login successful: "+username+" at "+r.URL.Path)
 		fmt.Println("Login successful")
+
 	}
 }
