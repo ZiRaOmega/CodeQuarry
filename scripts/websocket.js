56cd694ef213adcc637058edd773794f859fcf52
Svalbard <svalbardmalfant@gmail.com>
2024-04-25 17:09:22 +0200

    ___

--- scripts/websocket.js
+++ scripts/websocket.js
@@ -3,8 +3,8 @@ var socket;
 const getCookie = (name) => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
+    if (parts.length === 2) return parts.pop().split(";").shift();
+};
 // Use jQuery's document ready function to ensure the DOM is fully loaded
 $(document).ready(function () {
     // Create a WebSocket connection to the current hostname with the '/ws' endpoint
@@ -21,7 +21,7 @@ $(document).ready(function () {
         // Send a message to the server to check the session
         let message = {
             type: "session",
-            content: getCookie("session")
+            content: getCookie("session"),
         };
         socket.send(JSON.stringify(message));
         //socket.send("Hey there from client");
@@ -32,16 +32,16 @@ $(document).ready(function () {
         let msg = JSON.parse(event.data);
         switch (msg.type) {
             case "session":
-                if (msg.content=="expired" || msg.content=="empty"){
+                if (msg.content == "expired" || msg.content == "empty") {
                     console.log(`Session ${msg.content}, redirecting to login page`);
                     if (window.location.pathname != "/") window.location.href = "/";
-                    
-                }else if (msg.content=="valid"){
+                } else if (msg.content == "valid") {
                     // !!! TODO show the website if the session is still valid
-                    if (window.location.pathname != "/codeQuarry") window.location.pathname="/codeQuarry"
+                    if (window.location.pathname != "/codeQuarry")
+                        window.location.pathname = "/codeQuarry";
                     console.log("Session still valid");
                 }
-                }
+        }
         // Log the message received from the server
         console.log(`[message] Data received from server: ${event.data}`);
     };
@@ -51,10 +51,12 @@ $(document).ready(function () {
         // Check if the connection was closed cleanly
         if (event.wasClean) {
             // Log a message indicating the connection was closed cleanly
-            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            console.log(
+                `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+            );
         } else {
             // Log an error message indicating the connection was not closed cleanly
-            console.error('[close] Connection died');
+            console.error("[close] Connection died");
         }
     };
 
