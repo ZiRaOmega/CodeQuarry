3757c7bf39f29f44e85b1aec2027c6851411af47
DarylParisi <pharisidharyl_10@yahoo.fr>
2024-04-27 02:22:34 +0200

    hightlight_post_repared

--- app/db.go
+++ app/db.go
@@ -100,7 +100,7 @@ func createTableQuestion(db *sql.DB) {
 	tableCreationQuery := `CREATE TABLE IF NOT EXISTS Question(
 		id_question SERIAL NOT NULL,
 		title VARCHAR(50) NOT NULL,
-		content VARCHAR(500) NOT NULL,
+		content VARCHAR(10000) NOT NULL,
 		upvotes INT,
 		downvotes INT,
 		creation_date DATE NOT NULL,
@@ -253,7 +253,6 @@ func getUserIDFromDB(username string, db *sql.DB) (int, error) {
 	}
 	return id, nil
 }
-
 
 func getUserIDUsingSessionID(sessionID string, db *sql.DB) (int, error) {
 	var id int
--- scripts/detectLanguage.js
+++ scripts/detectLanguage.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all code blocks
-  document.querySelectorAll("pre code").forEach((block) => {
-    // Highlight.js detects the language
-    hljs.highlightElement(block);
-    // Extract the detected language
-    let detectedLanguage = block.classList.value.match(/language-(\w+)/)[1];
+function checkHighlight() {
+  // Ensure both hljs and Prism are defined
+  if (typeof hljs === "undefined") {
+    console.error("Highlight.js not found!");
+    return;
+  }
 
-    // Now set the Prism class for that language
-    block.className = "";
-    block.classList.add("language-" + detectedLanguage);
-    // Use Prism to highlight
-    Prism.highlightElement(block);
+  document.querySelectorAll("pre code").forEach((block) => {
+    // Apply Highlight.js
+    hljs.highlightElement(block);
   });
-});
+}
+
+// Call checkHighlight when the DOM is fully loaded and after any AJAX content load
+document.addEventListener("DOMContentLoaded", checkHighlight);
