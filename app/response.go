3e4b3ab0fa1c5735df927b8c4c235065a97b4af9
maximediet <maximediet>
2024-04-27 18:39:45 +0200

    question viewer

--- app/response.go
+++ app/response.go
@@ -48,6 +48,49 @@ func ResponsesHandler(db *sql.DB) http.HandlerFunc {
 		}
 	}
 }
+func QuestionViewerHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		questionID := r.URL.Query().Get("question_id")
+		if questionID == "" {
+			http.Error(w, "Missing question_id parameter", http.StatusBadRequest)
+			return
+		}
+		idint, err := strconv.Atoi(questionID)
+		if err != nil {
+			http.Error(w, "Invalid question_id parameter", http.StatusBadRequest)
+			return
+		}
+		questions, err := FetchQuestionByQuestionID(db, idint)
+		if err != nil {
+			http.Error(w, "Error fetching responses", http.StatusInternalServerError)
+			return
+		}
+		err = ParseAndExecuteTemplate("question_viewer", questions, w)
+		if err != nil {
+			http.Error(w, "Error parsing template", http.StatusInternalServerError)
+			return
+
+		}
+	}
+
+}
+func FetchQuestionByQuestionID(db *sql.DB, questionID int) (Question, error) {
+	var q Question
+	query := `SELECT q.id_question, s.title AS subject_title, q.title, q.description, q.content, q.creation_date, u.username, q.upvotes, q.downvotes
+				  FROM question q
+				  JOIN users u ON q.id_student = u.id_student	
+				  JOIN subject s ON q.id_subject = s.id_subject
+				  WHERE q.id_question = $1`
+	err := db.QueryRow(query, questionID).Scan(&q.Id, &q.SubjectTitle, &q.Title, &q.Description, &q.Content, &q.CreationDate, &q.Creator, &q.Upvotes, &q.Downvotes)
+	if err != nil {
+		return q, err
+	}
+	q.Responses, err = FetchResponseByQuestion(db, q.Id)
+	if err != nil {
+		return q, err
+	}
+	return q, nil
+}
 func FetchResponseByQuestion(db *sql.DB, questionID int) ([]Response, error) {
 	var responses []Response
 	query := `SELECT * FROM Response WHERE id_question = $1`
