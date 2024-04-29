79750067cf0c4fce33e12f12d388536bb890a858
DarylParisi <pharisidharyl_10@yahoo.fr>
2024-04-29 18:34:37 +0200

    injections_stopped

--- public/components/question_viewer/question_viewer.js
+++ public/components/question_viewer/question_viewer.js
@@ -43,25 +43,50 @@ const upvoteContainer = document.querySelectorAll(".upvote_container");
 const downvoteContainer = document.querySelectorAll(".downvote_container");
 const upvoteCount = document.querySelectorAll(".upvote_count");
 const downvoteCount = document.querySelectorAll(".downvote_count");
+const question_viewer__question__title = document.querySelector(
+  ".question-viewer__question__title"
+);
+
+const question_viewer__question__description = document.querySelector(
+  ".question-viewer__question__description"
+);
+
+const question_viewer__question__content = document.querySelector(
+  ".question-viewer__question__content"
+);
+
+const question_viewer__answers = document.querySelector(
+  ".question-viewer__answers"
+);
+
+const preDiv = document.createElement("pre");
+const code = document.createElement("code");
+preDiv.appendChild(code);
 
 fetch("/api/questions?subjectId=all")
   .then((response) => response.json())
   .then((data) => {
     data.forEach((question) => {
-      upvoteCount[0].setAttribute("data-question-id", question.id);
-      downvoteCount[0].setAttribute("data-question-id", question.id);
-      upvoteCount[0].textContent = question.upvotes;
-      downvoteCount[0].textContent = question.downvotes;
       //get the question where the id is the same as the one in the url
       if (question.id == getUrlArgument("question_id")) {
+        question_viewer__question__title.innerText = question.title;
+        question_viewer__question__description.innerText = question.description;
+        code.textContent = question.content;
+        question_viewer__question__content.appendChild(preDiv);
+        document.querySelectorAll("pre code").forEach((block) => {
+          // Apply Highlight.js
+          hljs.highlightElement(block);
+        });
+        upvoteCount[0].setAttribute("data-question-id", question.id);
+        downvoteCount[0].setAttribute("data-question-id", question.id);
+        upvoteCount[0].textContent = question.upvotes;
+        downvoteCount[0].textContent = question.downvotes;
         if (question.user_vote == "upvoted") {
           upvoteContainer[0].style.backgroundColor = "rgb(104, 195, 163)";
         } else if (question.user_vote == "downvoted") {
           downvoteContainer[0].style.backgroundColor = "rgb(196, 77, 86)";
         }
-
         upvoteContainer[0].onclick = function () {
-          upvoteCount[0].textContent = data.upvotes;
           //if upvoteContainer backgroundColor is green then remove the color
           if (
             upvoteContainer[0].style.backgroundColor == "rgb(104, 195, 163)"
@@ -86,7 +111,6 @@ fetch("/api/questions?subjectId=all")
         };
 
         downvoteContainer[0].onclick = function () {
-          downvoteCount[0].textContent = data.downvotes;
           //if downvoteContainer backgroundColor is red then remove the color
           if (
             downvoteContainer[0].style.backgroundColor == "rgb(196, 77, 86)"
@@ -108,6 +132,83 @@ fetch("/api/questions?subjectId=all")
             })
           );
         };
+        question.responses.forEach((answer) => {
+          const question_viewer__answers__answer =
+            document.createElement("div");
+          const question_viewer__answers__answer__description =
+            document.createElement("div");
+          const question_viewer__answers__answer__content =
+            document.createElement("div");
+          const question_viewer__answers__answer__date =
+            document.createElement("div");
+          const creator_name = document.createElement("span");
+          const creator_and_date_container = document.createElement("div");
+          const pre = document.createElement("pre");
+          const code = document.createElement("code");
+          creator_and_date_container.classList.add(
+            "creator_and_date_container"
+          );
+          creator_name.classList.add("creator_name");
+          question_viewer__answers__answer__date.classList.add(
+            "question-viewer__answers__answer__date"
+          );
+          const question_viewer__answers__answer__author =
+            document.createElement("div");
+          question_viewer__answers__answer__author.classList.add(
+            "question-viewer__answers__answer__author"
+          );
+          question_viewer__answers__answer__author.innerText = "Réponse de ";
+          creator_name.innerText = answer.student_name;
+          question_viewer__answers__answer__content.classList.add(
+            "question-viewer__answers__answer__content"
+          );
+          question_viewer__answers__answer__content.classList.add(
+            "question-viewer__answers__answer__content"
+          );
+          question_viewer__answers__answer.classList.add(
+            "question-viewer__answers__answer"
+          );
+          question_viewer__answers__answer__description.classList.add(
+            "question-viewer__answers__answer__description"
+          );
+
+          question_viewer__answers__answer__date.innerText = `Publié le: ${new Date(
+            question.creation_date
+          ).toLocaleDateString()}`;
+
+          question_viewer__answers__answer__description.innerText =
+            answer.description;
+
+          code.textContent = answer.content;
+
+          question_viewer__answers__answer__author.appendChild(creator_name);
+          pre.appendChild(code);
+          question_viewer__answers__answer__content.appendChild(pre);
+          question_viewer__answers__answer.appendChild(
+            question_viewer__answers__answer__description
+          );
+          question_viewer__answers__answer.appendChild(
+            question_viewer__answers__answer__content
+          );
+          question_viewer__answers.appendChild(
+            question_viewer__answers__answer
+          );
+          question_viewer__answers__answer.appendChild(
+            question_viewer__answers__answer__author
+          );
+          question_viewer__answers__answer.appendChild(
+            creator_and_date_container
+          );
+          creator_and_date_container.appendChild(
+            question_viewer__answers__answer__date
+          );
+          creator_and_date_container.appendChild(
+            question_viewer__answers__answer__author
+          );
+          document.querySelectorAll("pre code").forEach((block) => {
+            hljs.highlightElement(block);
+          });
+        });
       }
     });
   })
