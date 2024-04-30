5dfaed6de2b5a94e521be2980c35fd3b38ccde6b
Duvi <evan.duvivier@hotmail.com>
2024-04-30 17:32:19 +0200

    blank fills in the form won't appear on the profile page

--- public/components/profile/profile.js
+++ public/components/profile/profile.js
@@ -1,12 +1,33 @@
 const profileInfos = document.getElementById("profileInformations");
 const profileForm = document.getElementById("editProfileForm");
 const button = document.getElementById("editButton");
+const lines = document.getElementsByClassName("informations")
+const links = document.getElementsByClassName("links")
 
 const birthDate = document.getElementById("birthDate");
+const schoolDate = document.getElementById("schoolYear")
 
 if (birthDate.textContent == " 01/01/0001") {
 	birthDate.style.display = "none";
 }
+
+if (schoolDate.textContent == " 01/01/0001") {
+	schoolDate.style.display = "none";
+}
+
+for (let j of links) {
+	console.log(j.href)
+	if (j.href == "https://localhost/profile") {
+		j.style.display = "none";
+	}
+}
+
+for (let i of lines){
+	console.log(i.textContent)
+	if (i.textContent == "") {
+		i.style.display = "none"
+	}
+} 
 
 button.addEventListener("click", function () {
 	profileInfos.style.display = "none";
