95fa438260f66b4c0b47e8b0c423bacc51c4a1be
DarylParisi <pharisidharyl_10@yahoo.fr>
2024-05-02 17:06:44 +0200

    classement

--- app/classement.go
+++ app/classement.go
@@ -32,7 +32,7 @@ func ClassementHandler(db *sql.DB) http.HandlerFunc {
 func GetUsersInfo(db *sql.DB) ([]User, error) {
 	var users []User
 	query := `
-	SELECT id_student, username, xp
+	SELECT id_student, lastname, firstname, username, xp
 	FROM users
 	ORDER BY xp DESC`
 	rows, err := db.Query(query)
@@ -41,13 +41,19 @@ func GetUsersInfo(db *sql.DB) ([]User, error) {
 	}
 	for rows.Next() {
 		var user User
-		if err := rows.Scan(&user.ID, &user.Username, &user.XP); err != nil {
+		if err := rows.Scan(&user.ID, &user.Username, &user.LastName, &user.FirstName, &user.XP); err != nil {
 			return nil, err
 		}
 		user.Rank.String, err = SetRankByXp(user)
 		if err != nil {
 			return nil, err
 		}
+		Posts, err := FetchQuestionsByUserID(db, user.ID)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		user.My_Post = Posts
 		users = append(users, user)
 	}
 	defer rows.Close()
@@ -62,7 +68,6 @@ func SendUsersInfoJson(db *sql.DB) http.HandlerFunc {
 			http.Error(w, "Error getting users info", http.StatusInternalServerError)
 			return
 		}
-		fmt.Println(users)
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(users)
 	}
