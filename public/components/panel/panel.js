3e1446513916e2c57bb9395116dbb970f9dce7af
maximediet <maximediet>
2024-05-07 23:51:49 +0200

    edit delete for all complete

--- public/components/panel/panel.js
+++ public/components/panel/panel.js
@@ -90,3 +90,87 @@ function addSubject(){
     textareas.value = "";
 
 }
+
+function deleteSubject(id){
+    const data = {
+        type: "deleteSubjectPanel",
+        content: {
+            id: id,
+        },
+        session_id: getCookie("session")
+    };
+    socket.send(JSON.stringify(data));
+}
+
+function deleteQuestion(id){
+    const data = {
+        type: "deleteQuestionPanel",
+        content: {
+            id: id,
+        },
+        session_id: getCookie("session")
+    };
+    socket.send(JSON.stringify(data));
+}
+function deleteResponse(id,question_id){
+    const data = {
+        type: "deleteResponsePanel",
+        content: {
+            id: id,
+            question_id: question_id
+        },
+        session_id: getCookie("session")
+    };
+    socket.send(JSON.stringify(data));
+}
+
+function editUser(id){
+    /** <div class="user_details" data-user-id="{{.ID}}">
+                <input class="input-field" type="text" value="{{.FirstName}} " /><br>
+                <input class="input-field" type="text" value="{{.LastName}}" /><br>
+                <input class="input-field" type="text" value="@{{.Username}}" /><br>
+                <input class="input-field" type="email" value="{{.Email}}" /><br>
+                <textarea class="textarea-field">{{if .Bio.Valid}}{{.Bio.String}}{{else}}Not provided{{end}}</textarea><br>
+                <input class="input-field" type="url" value="{{if .Website.Valid}}{{.Website.String}}{{end}}" /><br>
+                <input class="input-field" type="url" value="{{if .GitHub.Valid}}{{.GitHub.String}}{{end}}" /><br>
+                <input class="input-field" type="number" value="{{if .XP.Valid}}{{.XP.Int64}}{{else}}0{{end}}" /><br>
+                <input class="input-field" type="text" value="{{if .Rank.Valid}}{{.Rank.String}}{{else}}Unranked{{end}}" /><br>
+                <input class="input-field" type="date" value="{{if .SchoolYear.Valid}}{{.SchoolYear.Time}}{{end}}" /><br>
+                <button >Edit</button>
+                <button >Delete</button>
+            </div>*/
+    const userElement = document.querySelector(`[data-user-id="${id}"]`);
+    const inputs = userElement.querySelectorAll('.input-field');
+    const textareas = userElement.querySelectorAll('.textarea-field');
+    //Create data object
+    const data = {
+        type: "editUserPanel",
+        content: {
+            id: id,
+            firstname : inputs[0].value,
+            lastname : inputs[1].value,
+            username : inputs[2].value.replace("@",""),
+            email : inputs[3].value,
+            bio : textareas[0].value,
+            website : inputs[4].value,
+            github : inputs[5].value,
+            xp : inputs[6].value,
+            rank : inputs[7].value,
+            schoolyear : inputs[8].value
+        },
+        session_id: getCookie("session")
+    };
+    //Send data through WebSocket
+    socket.send(JSON.stringify(data));
+}
+
+function deleteUser(id){
+    const data = {
+        type: "deleteUserPanel",
+        content: {
+            id: id,
+        },
+        session_id: getCookie("session")
+    };
+    socket.send(JSON.stringify(data));
+}
