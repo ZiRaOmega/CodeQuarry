0a3eed35fe5921c5ceb44936e5b6d9467ecfb118
Svalbard <svalbardmalfant@gmail.com>
2024-05-15 15:06:53 +0200

    search

--- public/templates/search_bar/input.js
+++ public/templates/search_bar/input.js
@@ -1,82 +1,3 @@
-/* // {subject: "Math", marks:[
-let input = document.getElementById("search-bar-input");
-input.addEventListener("keyup", function () {
-  if (QuestionsElementsList.length==0){
-    fetchQuestions("all")
-  }
-  console.log("searching...");
-  //if there is no subjectId inside localstorage
-  if (
-    !localStorage.getItem("subjectId") ||
-    localStorage.getItem("subjectId") === ""
-  ) {
-    localStorage.setItem("subjectId", "all");
-    localStorage.setItem("subjectTitle", "All Subjects");
-    ListElement.style.display = "none"; // Hide the list
-    fetchQuestions("all"); // Fetch all questions
-  }
-  const search = input.value.toLowerCase();
-  for (let i = 0; i < QuestionsElementsList.length; i++) {
-    const questionElement = QuestionsElementsList[i];
-    const question_title =
-      questionElement.querySelector(".question_title").innerText;
-    const question_content =
-      questionElement.querySelector("pre code").innerText;
-    const question_description = questionElement.querySelector(
-      ".question_description"
-    ).innerText;
-    if (
-      question_title.toLowerCase().includes(search) ||
-      question_content.toLowerCase().includes(search) ||
-      question_description.toLowerCase().includes(search)
-    ) {
-      questionElement.style.display = "block";
-    } else {
-      questionElement.style.display = "none";
-    }
-  }
-});
-document.getElementById("home").addEventListener("click", function () {
-  localStorage.removeItem("subjectId");
-  localStorage.removeItem("subjectTitle");
-  ListElement.style.display = "";
-}); */
-
-// Same but display results in a scroll dynamic way
-let input = document.getElementById("search-bar-input");
-let search_results = document.getElementById("search_results");
-//search_results
-input.addEventListener("keyup", function () {
-  const search = input.value.toLowerCase();
-  search_results.innerHTML = "";
-  for (let i = 0; i < potential_results.length; i++) {
-    const question = potential_results[i];
-    const question_title = question.title;
-    const question_content = question.content;
-    const question_description = question.description;
-    const question_subject = question.subject_title;
-    if (search === "") {
-      search_results.innerHTML = "";
-    } else if (
-      question_title.toLowerCase().includes(search) ||
-      question_content.toLowerCase().includes(search) ||
-      question_description.toLowerCase().includes(search) ||
-      question_subject.toLowerCase().includes(search)
-    ) {
-      search_results.innerHTML += `
-      <div class="subject_tag">${question.subject_title}</div>
-      <div class="clickable_container">
-          <h3 class="question_title">${question.title}</h3>
-          <p class="question_description">${question.description}</p>
-          <pre><code>${question.content}</code></pre>
-      </div>
-      `;
-    }
-    // highlight search results
-    checkHighlight();
-  }
-});
-
 let potential_results = [];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -86,12 +7,84 @@ document.addEventListener("DOMContentLoaded", function () {
       const questions = await response.json();
       console.log(questions);
       potential_results = questions;
-
     } catch (error) {
-      console.error("There was a problem with your fetch operation:", error);
+      console.error(
+        "There was a problem with your fetch operation:",
+        error
+      );
     }
-  }
-  )();
-}
-);
+  })();
+});
 
+document
+  .getElementById("search_bar_input")
+  .addEventListener("input", function () {
+    var input = this.value.toLowerCase();
+    var results = document.getElementById("search_results");
+    results.innerHTML = "";
+
+    if (input) {
+      results.style.display = "block"; // Show dropdown
+      potential_results.forEach(function (item) {
+
+        var prefix = document.createElement("span");
+        prefix.className = "result_prefix";
+
+        if (item.title.toLowerCase().indexOf(input) > -1) {
+          prefix.textContent = "Title: ";
+        } else if (item.content.toLowerCase().indexOf(input) > -1) {
+          prefix.textContent = "Content: ";
+        } else if (item.description.toLowerCase().indexOf(input) > -1) {
+          prefix.textContent = "Description: ";
+        } else if (item.subject_title.toLowerCase().indexOf(input) > -1) {
+          prefix.textContent = "Subject: ";
+        } else {
+          return;
+        }
+        var rslt_element = document.createElement("div");
+        rslt_element.className = "result_element";
+
+
+        var preview = document.createElement("div");
+        preview.className = "result_element_preview";
+
+        var details = document.createElement("div");
+        details.className = "result_element_details";
+        details.innerHTML = `<div class="subject_tag">${item.subject_title}</div>
+            <h3 class="question_title">${item.title}</h3>
+            <p class="question_description">${item.description}</p>
+            <pre><code>${item.content}</code></pre>
+        `;
+
+
+        var result_text = document.createElement("span");
+        result_text.className = "result_text";
+        result_text.textContent = item.title;
+
+        preview.appendChild(prefix);
+        preview.appendChild(result_text);
+
+        rslt_element.appendChild(preview);
+        rslt_element.appendChild(details);
+
+
+        rslt_element.onclick = function () {
+          // Click on item to redirect to question_viewer page
+          window.location.href = `/question_viewer?question_id=7`;
+        };
+        results.appendChild(rslt_element);
+        checkHighlight();
+      });
+    } else {
+      results.style.display = "none"; // Hide dropdown if input is empty
+    }
+  });
+
+/*
+    <div class="subject_tag">${question.subject_title}</div>
+    <div class="clickable_container">
+        <h3 class="question_title">${question.title}</h3>
+        <p class="question_description">${question.description}</p>
+        <pre><code>${question.content}</code></pre>
+    </div>
+*/
