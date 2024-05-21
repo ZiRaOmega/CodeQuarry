36ea626a80e32a1ed43fd77983efdc7d15b4fc32
maximediet <maximediet>
2024-05-21 22:27:49 +0200

    renew cert

--- renew_certs.sh
+++ renew_certs.sh
@@ -3,8 +3,5 @@
 # Stop the application
 sudo docker-compose stop app
 
-# Run Certbot to renew the certificate
-sudo certbot certonly --standalone -d codequarry.dev --email maxime.diet76@gmail.com --agree-tos --non-interactive --keep; sudo cp -r /etc/letsencrypt/archive/codequarry.dev /cert
-
 # Start the application again
-screen -dmS codequarry sh -c 'sudo docker-compose build && sudo docker-compose up'
+screen -dmS codequarry sh -c 'sudo certbot certonly --standalone -d codequarry.dev --email maxime.diet76@gmail.com --agree-tos --non-interactive --keep; sudo cp -r /etc/letsencrypt/archive/codequarry.dev /cert && sudo docker-compose build && sudo docker-compose up'
