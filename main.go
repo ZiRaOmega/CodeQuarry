820a7cfa30a453faf406404b252d1be56d2c238c
maximediet <maximediet>
2024-05-26 19:02:42 +0200

    merge ersen

--- main.go
+++ main.go
@@ -162,6 +162,10 @@ func main() {
 	//go startHTTPServer()
 	fmt.Println("Server is running on https://" + URL + ":443/")
 	err = http.ListenAndServeTLS(":443", "./cert/fullchain1.pem", "./cert/privkey1.pem", nil)
+	//For the server
+	//err = http.ListenAndServeTLS(":443", "./cert/codequarry.dev/fullchain1.pem", "./cert/codequarry.dev/privkey1.pem", nil)
+
+
 	//err = http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
 	if err != nil {
 		app.Log(app.ErrorLevel, "Error starting the server")
