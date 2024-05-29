ed16bce56d8f492833b7827e4ecf3fdb1f5325b5
maximediet <maximediet>
2024-05-29 03:44:11 +0200

    remove unnecessary

--- public/components/home/votes.js
+++
@@ -1,9 +0,0 @@
-/* // Handling WebSocket messages for votes
-socket.onmessage = function (event) {
-  let msg = JSON.parse(event.data);
-  if (msg.type === "voteUpdate") {
-    console.log("Vote updated for question:", msg.content.questionId);
-    // Update UI accordingly
-  }
-};
- */
--- renew_certs.sh
+++ renew_certs.sh
@@ -6,4 +6,4 @@ sudo docker-compose stop app
 # Start the application again
 sudo certbot certonly --standalone -d codequarry.dev -d www.codequarry.dev --email maxime.diet76@gmail.com --agree-tos --non-interactive --keep 
 sudo cp -r /etc/letsencrypt/archive/codequarry.dev ./cert 
-sudo docker-compose start app'
+sudo docker-compose start app
