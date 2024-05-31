67f114181a0f79b35b2641272d321c61512e22f5
maximediet <maximediet>
2024-05-31 19:48:37 +0200

    renew cert cp changed

--- renew_certs.sh
+++ renew_certs.sh
@@ -5,5 +5,5 @@ sudo docker-compose stop app
 
 # Start the application again
 sudo certbot certonly --standalone --expand -d codequarry.dev -d www.codequarry.dev -d codequarry.ovh -d www.codequarry.ovh --email maxime.diet76@gmail.com --agree-tos --non-interactive --keep
-sudo cp -r /etc/letsencrypt/archive/codequarry.dev ./cert/codequarry.dev
+sudo cp -r /etc/letsencrypt/archive/codequarry.dev ./cert/
 sudo docker-compose start app
