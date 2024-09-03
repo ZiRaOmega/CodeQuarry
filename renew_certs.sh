c300ee7bee1b44d7f2719f4672a09d7c443ac0f7
ZiRaOmega <max.diet@free.fr>
2024-09-03 13:12:45 +0200

    fix renew

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
-
 # Stop the application
 sudo docker-compose stop app
-
+#Stop nginx
+sudo systemctl stop nginx
 # Start the application again
-sudo certbot certonly --standalone --expand -d domain_name -d domain_name_2 -d domaine_name_3 -d domaine_name_4 --email email_address --agree-tos --non-interactive --keep
-sudo cp -r /etc/letsencrypt/archive/domain_name ./cert/
+sudo certbot delete --cert-name domain.name --non-interactive
+sudo certbot certonly --standalone --expand -d domain.name -d www.domain.name -d domain.name -d domain.name --email email@adress.gg --agree-tos --no>
+sudo cp -r /etc/letsencrypt/archive/domain.name ./cert/
 sudo docker-compose start app
+
+#Restart nginx
+sudo systemctl start nginx
